b10c65b1be41009381ad85f07b38eb07fd9a81e5
Kartic Grover <kartic@google.com>
2012-11-30 12:03:40 +0530

    Updated refernces to Notepad Sample files
    Copy and Paste reference code in NotePad sample has been moved
    to different files hence changing their refernces in ClipData
    documentation
    
    Change-Id: I410100bd2c6a7780f33211264fe3b9677c70c153
    Signed-off-by: Kartic Grover <kartic@google.com>

--- core/java/android/content/ClipData.java
+++ core/java/android/content/ClipData.java
@@ -93,7 +93,7 @@ import java.util.ArrayList;
  * the entire structure of the note into a new note; otherwise, it simply
  * coerces the clip into text and uses that as the new note's contents.
  *
- * {@sample development/samples/NotePad/src/com/example/android/notepad/NoteEditor.java
+ * {@sample development/samples/NotePad/src/com/example/android/notepad/NoteEditorFragment.java
  *      paste}
  *
  * <p>In many cases an application can paste various types of streams of data.  For
@@ -141,7 +141,7 @@ import java.util.ArrayList;
  * <p>The copy operation in our NotePad application is now just a simple matter
  * of making a clip containing the URI of the note being copied:
  *
- * {@sample development/samples/NotePad/src/com/example/android/notepad/NotesList.java
+ * {@sample development/samples/NotePad/src/com/example/android/notepad/NotesListFragment.java
  *      copy}
  *
  * <p>Note if a paste operation needs this clip as text (for example to paste
