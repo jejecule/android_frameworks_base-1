a2ee2547993c522f16379a87c0531b35ceb5bd37
Lars Greiss <kufikugel@googlemail.com>
2013-12-01 14:14:38 +0100

    Frameworks: [1/2] mute notification sounds threshold
    add back the 4.3 feature like it was.
    User can setup how often notification sound
    for an app comes trough so that
    it does not annoy to much
    
    Change-Id: I3d7b2ef2e2ff99b7c23880626e0268ec4c5d8543

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3205,6 +3205,13 @@ public final class Settings {
         public static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";
 
         /**
+         * Whether to mute annoying notifications
+         * @hide
+         */
+        public static final String MUTE_ANNOYING_NOTIFICATIONS_THRESHOLD =
+                "mute_annoying_notifications_threshold";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
