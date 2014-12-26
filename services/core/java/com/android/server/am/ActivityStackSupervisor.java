d329fc62ca28150fffdc36761e5cb35e082d42ab
Pracheer <pracheer@codeaurora.org>
2014-12-26 13:35:03 +0530

    perf: Boost packing parameters during launch
    Add support for boosting packing parameters
    small_task, mostly_idle_load and mostly_idle_nr_run
    during first launch.
    The desired values can be defined in device specific
    config files
    
    Change-Id: I8f9c903697632d465e86351cf4c2c723b5d141eb
    CRs-Fixed: 796165

--- services/core/java/com/android/server/am/ActivityStackSupervisor.java
+++ services/core/java/com/android/server/am/ActivityStackSupervisor.java
@@ -154,6 +154,9 @@ public final class ActivityStackSupervisor implements DisplayListener {
     public int lBoostSchedBoost = 0;
     public int lBoostPcDisblBoost = 0;
     public int lBoostKsmBoost = 0;
+    public int lBoostSmTaskBoost = 0;
+    public int lBoostIdleLoadBoost = 0;
+    public int lBoostIdleNrRunBoost = 0;
     public int lBoostPreferIdle = 0;
     static final int HANDLE_DISPLAY_ADDED = FIRST_SUPERVISOR_STACK_MSG + 5;
     static final int HANDLE_DISPLAY_CHANGED = FIRST_SUPERVISOR_STACK_MSG + 6;
@@ -331,9 +334,14 @@ public final class ActivityStackSupervisor implements DisplayListener {
                    com.android.internal.R.integer.launchboost_pcdisbl_param);
            lBoostKsmBoost = mService.mContext.getResources().getInteger(
                    com.android.internal.R.integer.launchboost_ksmboost_param);
+           lBoostSmTaskBoost = mService.mContext.getResources().getInteger(
+                   com.android.internal.R.integer.launchboost_smtaskboost_param);
+           lBoostIdleLoadBoost = mService.mContext.getResources().getInteger(
+                   com.android.internal.R.integer.launchboost_idleloadboost_param);
+           lBoostIdleNrRunBoost = mService.mContext.getResources().getInteger(
+                   com.android.internal.R.integer.launchboost_idlenrrunboost_param);
            lBoostPreferIdle = mService.mContext.getResources().getInteger(
                    com.android.internal.R.integer.launchboost_preferidle_param);
-
        }
     }
 
@@ -2789,7 +2797,8 @@ public final class ActivityStackSupervisor implements DisplayListener {
         }
         if (mPerf != null) {
             mPerf.perfLockAcquire(lBoostTimeOut, lBoostPcDisblBoost, lBoostSchedBoost,
-                                  lBoostCpuBoost, lBoostKsmBoost, lBoostPreferIdle);
+                                  lBoostCpuBoost, lBoostKsmBoost, lBoostSmTaskBoost,
+                                  lBoostIdleLoadBoost, lBoostIdleNrRunBoost,lBoostPreferIdle);
         }
 
 
