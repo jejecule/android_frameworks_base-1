2f6f27a99a122ea035f61002b165bf571e2085ea
d34d <clark@cyngn.com>
2015-02-15 10:21:42 -0800

    Themes: Fix theme package name being set to target's
    The themePackageName was being set to the target's base package
    name instead of the theme's package name.
    
    Change-Id: I8875409d3ee302ae7940cab6d82cb4265eab54e3

--- core/java/android/app/ResourcesManager.java
+++ core/java/android/app/ResourcesManager.java
@@ -562,7 +562,7 @@ public class ResourcesManager {
             return false;
         }
 
-        String themePackageName = basePackageName;
+        String themePackageName = piTheme.packageName;
         String themePath = piTheme.applicationInfo.publicSourceDir;
         if (!piTarget.isThemeApk && piTheme.mOverlayTargets.contains(basePackageName)) {
             String targetPackagePath = piTarget.applicationInfo.sourceDir;
@@ -575,7 +575,7 @@ public class ResourcesManager {
                     targetPackagePath, prefixPath);
 
             if (cookie != 0) {
-                assets.setThemePackageName(basePackageName);
+                assets.setThemePackageName(themePackageName);
                 assets.addThemeCookie(cookie);
             }
         }
