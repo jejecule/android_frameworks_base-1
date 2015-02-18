dfa1c4775d9bb9b88aa67f512d8d945af55c6d16
Steve Kondik <steve@cyngn.com>
2015-02-18 03:52:24 -0800

    Squashed commits for livedisplay support
    * Based on AOSPA's commit
    --
    Credits to : CM and AOSPA
    OG COMMIT : https://github.com/AOSPA-L/android_frameworks_base/commit/b8ab2a6715375c489c86d6d8887c529b0c4d6d65
    
    livedisplay: Implement active display adjustment
    
     * LiveDisplay is our new name for the various display technologies
       which adjust the screen based on environmental conditions other
       than the standard automatic backlight controls.
     * This patch implements automatic color temperature adjustment based
       on time of day. This is similar to f.lux or Redshift. My eyes are
       so happy now!
     * Automatic outdoor/SRE feature is now handled here.
     * Handling of CABC/CABL and color enhancements also handled here.
     * Manual RGB tweaking is handled here.
     * Can delegate to DisplayColor HAL if available, otherwise uses
       SurfaceFlinger's 1015 operation to apply changes.
     * Happily coexists with the new accessibility features for color
       blindness correction and color inversion.
     * All postprocessing will be disabled when powersave mode is
       activated.
    
    Change-Id: Iac1b74f410957f8e2d8290465c4ce9cc1fd97a88
    
    livedisplay: Fix color calibration
    
     * Color calibration was not applied when the color temperature wasn't
       being adjusted as well. Fixit.
    
    Change-Id: I69d11359088db088f5f1fd9bd1d3abfa54cc50c2
    
    livedisplay: Add "off" state and fix issues with QS tile
    
     * It was previously assumed that "day" was the default setting that
       would be used the same as "off" would, but this is confusing and
       also doesn't allow for a custom day temperature.
     * Add a new state for "off" to simplify this, and handle it
       appropriately. We won't show "day" in the QS tile if both "off" and
       "day" are set to 6500K.
     * Also fix bugs in the QS tile.
    
    Change-Id: Ie8e6ebdd83c9275fb7707844b8f054dcbff48a0b
    
    livedisplay: Fix issues when using multiple users
    
    Change-Id: Id0732b23aa324e92c6d0aaa17e64e3c76cd89450
    
    livedisplay: Updated icon from Asher
    
    Change-Id: I093eb6925b939fa37f0d67d2a42b24fa2494b98c
    
    livedisplay: Don't depend on automatic brightness
    
     * Some devices will have this disabled or unavailable. Create the
       controller from DisplayPowerManager and give it a kick during
       screen power changes.
    
    Change-Id: I293100a29cfcc342eef6369dcee76261b5a89f3a
    
    Fix LiveDisplay notification.
    
    Increase the size of the notification icon, as it was displaying
    too small. Also added the ability to expand the notification so
    that the entire string can be seen.
    
    Change-Id: Ic071ed0710548625b4158129aeaa5a7b3d62b4fc
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	core/res/res/values/pa_strings.xml
    	core/res/res/values/pa_symbols.xml

--- services/core/java/com/android/server/display/AutomaticBrightnessController.java
+++ services/core/java/com/android/server/display/AutomaticBrightnessController.java
@@ -21,6 +21,8 @@ import com.android.server.twilight.TwilightListener;
 import com.android.server.twilight.TwilightManager;
 import com.android.server.twilight.TwilightState;
 
+import android.content.Context;
+import android.content.res.Resources;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -179,11 +181,18 @@ class AutomaticBrightnessController {
     // Are we going to adjust brightness while dozing.
     private boolean mDozing;
 
-    public AutomaticBrightnessController(Callbacks callbacks, Looper looper,
+    // Night mode color temperature adjustments
+    private final LiveDisplayController mLiveDisplay;
+
+    private final Context mContext;
+
+    public AutomaticBrightnessController(Context context, Callbacks callbacks, Looper looper,
             SensorManager sensorManager, Spline autoBrightnessSpline, int lightSensorWarmUpTime,
             int brightnessMin, int brightnessMax, float dozeScaleFactor,
             int lightSensorRate, long brighteningLightDebounceConfig,
-            long darkeningLightDebounceConfig, boolean resetAmbientLuxAfterWarmUpConfig) {
+            long darkeningLightDebounceConfig, boolean resetAmbientLuxAfterWarmUpConfig,
+            LiveDisplayController ldc) {
+        mContext = context;
         mCallbacks = callbacks;
         mTwilight = LocalServices.getService(TwilightManager.class);
         mSensorManager = sensorManager;
@@ -196,6 +205,7 @@ class AutomaticBrightnessController {
         mBrighteningLightDebounceConfig = brighteningLightDebounceConfig;
         mDarkeningLightDebounceConfig = darkeningLightDebounceConfig;
         mResetAmbientLuxAfterWarmUpConfig = resetAmbientLuxAfterWarmUpConfig;
+        mLiveDisplay = ldc;
 
         mHandler = new AutomaticBrightnessHandler(looper);
         mAmbientLightRingBuffer = new AmbientLightRingBuffer(mLightSensorRate);
@@ -459,6 +469,9 @@ class AutomaticBrightnessController {
                 Slog.d(TAG, "updateAutoBrightness: adjGamma=" + adjGamma);
             }
         }
+
+        // Update LiveDisplay with the current lux
+        mLiveDisplay.updateLiveDisplay(mAmbientLux);
 
         if (USE_TWILIGHT_ADJUSTMENT) {
             TwilightState state = mTwilight.getCurrentState();
