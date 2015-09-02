7b516670eb59a33912c329b80e593eb78c87a8ae
mkodekar <mkodekar@gmail.com>
2015-09-02 10:18:01 +0200

    Revert "Themes: Theme power off and reboot dialog"
    This reverts commit ae23a134c6b74001b28b188b5eed4fd5620b9913.

--- services/core/java/com/android/server/power/ShutdownThread.java
+++ services/core/java/com/android/server/power/ShutdownThread.java
@@ -134,10 +134,9 @@ public final class ShutdownThread extends Thread {
      * @param confirm true if user confirmation is needed before shutting down.
      */
     public static void shutdown(final Context context, boolean confirm) {
-        final Context uiContext = getUiContext(context);
         mReboot = false;
         mRebootSafeMode = false;
-        shutdownInner(uiContext, confirm);
+        shutdownInner(context, confirm);
     }
 
     static void shutdownInner(final Context context, boolean confirm) {
@@ -267,11 +266,10 @@ public final class ShutdownThread extends Thread {
      * @param confirm true if user confirmation is needed before shutting down.
      */
     public static void reboot(final Context context, String reason, boolean confirm) {
-        final Context uiContext = getUiContext(context);
         mReboot = true;
         mRebootSafeMode = false;
         mRebootReason = reason;
-        shutdownInner(uiContext, confirm);
+        shutdownInner(context, confirm);
     }
 
     private static String getShutdownMusicFilePath() {
